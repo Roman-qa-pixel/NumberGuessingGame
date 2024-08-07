1ba6f212e94a70fcde88c800389c940eb9af57b3
nkturtas@gmx.com <nkturtas@gmx.com>
2024-08-07 09:32:57 +0300

    Add project files.

---
+++ NumberGuessingGame/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace NumberGuessingGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guessButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numberLabel = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // guessButton
+            // 
+            this.guessButton.Font = new System.Drawing.Font("Myanmar Text", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guessButton.Location = new System.Drawing.Point(126, 242);
+            this.guessButton.Name = "guessButton";
+            this.guessButton.Size = new System.Drawing.Size(125, 45);
+            this.guessButton.TabIndex = 0;
+            this.guessButton.Text = "Guess";
+            this.guessButton.UseVisualStyleBackColor = true;
+            this.guessButton.Click += new System.EventHandler(this.guessButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(126, 163);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // numberLabel
+            // 
+            this.numberLabel.Font = new System.Drawing.Font("Myanmar Text", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numberLabel.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.numberLabel.Location = new System.Drawing.Point(50, 28);
+            this.numberLabel.Name = "numberLabel";
+            this.numberLabel.Size = new System.Drawing.Size(288, 121);
+            this.numberLabel.TabIndex = 2;
+            this.numberLabel.Text = "Guess A Number";
+            this.numberLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Myanmar Text", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.Location = new System.Drawing.Point(286, 279);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(62, 30);
+            this.scoreLabel.TabIndex = 3;
+            this.scoreLabel.Text = "Score:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cyan;
+            this.ClientSize = new System.Drawing.Size(383, 328);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.numberLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.guessButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button guessButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label numberLabel;
+        private System.Windows.Forms.Label scoreLabel;
+    }
+}
+
---
+++ NumberGuessingGame/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberGuessingGame
+{
+    public partial class Form1 : Form
+    {
+        Random r = new Random();
+        int currentScore = 0;
+        string guess = "";
+        public Form1()
+        {
+            InitializeComponent();
+            this.AcceptButton = guessButton;
+            scoreLabel.Text = "Score: " + currentScore.ToString();
+        }
+
+        private void guessButton_Click(object sender, EventArgs e)
+        {
+            guess = textBox1.Text;
+            checkWinner(guess);
+        }
+
+        private void checkWinner(string guess)
+        {
+            try
+            {
+                int numToGuess = r.Next(1, 10);
+                int guessedNum = int.Parse(guess);
+
+                if (numToGuess == guessedNum)
+                {
+                    //We have won the game
+                    currentScore++;
+                    numberLabel.Text = "You have won the game! " + guessedNum;
+                    textBox1.Text = "";
+                    scoreLabel.Text = currentScore.ToString();
+                }
+
+                else
+                {
+                    //We have not won the game
+                    numberLabel.Text = "You have lost the game! " + guessedNum;
+                    textBox1.Text = "";
+                }
+            }
+            catch (Exception e)
+            {
+                numberLabel.Text = "Error!";
+                textBox1.Text = "";
+            
+            }
+        }
+    }
+}
